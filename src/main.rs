0977744cda799ac1eb089e0a880f6f4652a8471d
Yuval Langer <yuval.langer@gmail.com>
2015-06-11 00:16:00 +0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate rand;
+
+use std::io::Read;
+use rand::Rng;
+
+fn main() {
+    let beale_contents = include_str!("../beale.wordlist.asc");
+
+    let v: Vec<_> = beale_contents.split('\n')
+        .skip(2)
+        .take(7776)
+        .map(|s| s.splitn(2, '\t').nth(1).unwrap())
+        .collect();
+
+    let mut rng = rand::OsRng::new().unwrap();
+    for _ in 0..7 {
+        let c = rng.choose(&v);
+        print!("{} ", c.unwrap());
+    }
+    println!("");
+}
