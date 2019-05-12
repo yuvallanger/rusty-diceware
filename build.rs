9fcdee12859b0200c6a6b613c0ad048e73466b81
Yuval Langer <yuval.langer@gmail.com>
2019-05-12 20:33:42 +0300

    Apply `cargo fmt`.

--- build.rs
+++ build.rs
@@ -1,13 +1,13 @@
 use std::env;
 use std::fs::File;
-use std::io::Write;
 use std::io::Read;
+use std::io::Write;
 use std::path::Path;
 use std::string::String;
 
-
 fn make_wordlist(contents: &std::string::String) -> Vec<&str> {
-    contents.split('\n')
+    contents
+        .split('\n')
         .skip(2)
         .take(7776)
         .map(|s| s.splitn(2, '\t').nth(1).unwrap())
@@ -83,30 +83,34 @@ fn main() {
     println!("{}", manifest_dir);
     let wordlists_dir = Path::new(&manifest_dir).join("bin").join("wordlists");
     let mut beale_wordlist_file = File::open(&wordlists_dir.join("beale.wordlist.asc")).unwrap();
-    let mut reinhold_wordlist_file = File::open(&wordlists_dir.join("diceware.wordlist.asc")).unwrap();
+    let mut reinhold_wordlist_file =
+        File::open(&wordlists_dir.join("diceware.wordlist.asc")).unwrap();
     let mut minilock_wordlist_file = File::open(&wordlists_dir.join("phrase.js")).unwrap();
 
     let mut beale_wordlist_string = String::new();
     let mut reinhold_wordlist_string = String::new();
     let mut minilock_wordlist_string = String::new();
 
-    beale_wordlist_file.read_to_string(&mut beale_wordlist_string).unwrap();
-    reinhold_wordlist_file.read_to_string(&mut reinhold_wordlist_string).unwrap();
-    minilock_wordlist_file.read_to_string(&mut minilock_wordlist_string).unwrap();
+    beale_wordlist_file
+        .read_to_string(&mut beale_wordlist_string)
+        .unwrap();
+    reinhold_wordlist_file
+        .read_to_string(&mut reinhold_wordlist_string)
+        .unwrap();
+    minilock_wordlist_file
+        .read_to_string(&mut minilock_wordlist_string)
+        .unwrap();
 
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("diceware.rs");
     let mut f = File::create(&dest_path).unwrap();
 
-    f.write_all(
-        make_beale_struct(make_wordlist(&beale_wordlist_string)).as_bytes()
-    ).unwrap();
+    f.write_all(make_beale_struct(make_wordlist(&beale_wordlist_string)).as_bytes())
+        .unwrap();
 
-    f.write_all(
-        make_reinhold_struct(make_wordlist(&reinhold_wordlist_string)).as_bytes()
-    ).unwrap();
+    f.write_all(make_reinhold_struct(make_wordlist(&reinhold_wordlist_string)).as_bytes())
+        .unwrap();
 
-    f.write_all(
-        make_minilock_struct(make_minilock_wordlist(&minilock_wordlist_string)).as_bytes()
-    ).unwrap();
+    f.write_all(make_minilock_struct(make_minilock_wordlist(&minilock_wordlist_string)).as_bytes())
+        .unwrap();
 }
